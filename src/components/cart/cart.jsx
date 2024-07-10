d6e9afa2dad0b5ec77ab859d666aa194b382674c
Oluwatosin <oluwatosin@gmail.com>
2024-07-10 22:51:01 +0100

    first commit

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -4,22 +4,15 @@ import search from "../../images/searchs.png";
 import cart from "../../images/carts.png";
 import { Link } from "react-router-dom";
 import golds from "../../images/Rectangle 35.png";
-import ring from "../../images/Rectangle 37.png";
-import brace from "../../images/Rectangle 38.png";
-import watches from "../../images/Rectangle 39.png";
-import bangles from "../../images/Rectangle 40.png";
-import pin from "../../images/Rectangle 41.png";
-import galaxy from "../../images/Rectangle 220.png";
-import cubana from "../../images/Rectangle 46.png";
-import love from "../../images/outlined.png";
-import facebook from "../../images/facebook.png";
-import instagram from "../../images/instagram.png";
+import rose from "../../images/Rectangle 34.jpg";
+
 
 
 export const CartPage = () => {
     return(
 
         <div>
+
             <header>
               <nav className="navbar">
                 <ul>
@@ -41,115 +34,66 @@ export const CartPage = () => {
                     <img src={cart} alt="image" />
                     </Link>
                   </li>
-
-                  <button className="btn">
-                    <a href="a">SIGN IN</a>
-                  </button>
                 </ul>
               </nav>
 
-             <div className="heading">
-               <h1>STRIDE DOWN YOUR MUSE</h1>
-             </div>
-
-            <div className="section">
-              <img src={golds} alt="image" />
-              <h1>GET ALL EXQUISITE AND LUXERY JEWELRY TO YOUR CONFIDENCE GOING</h1>
-              <button class="shop"><a href="a">SHOP NOW</a></button>
-              
-            </div>
-          </header>
-
-          <div className="sections">
-            <h1>BY PRODUCT</h1>
-
-            <div className="sect">
-              <div className="first-section">
-                <img src={ring} alt="image" />
-                <h3>RINGS</h3>
-            </div>
-
-              <div className="first-section">
-                <img src={brace} alt="image" />
-                <h3>EARRINGS</h3>
+              <div className="heading">
+                <div className="display">
+                  <h2>ORDER SUMMARY</h2>
+                  <p>1 Item</p>
+                </div>
               </div>
 
-              <div className="first-section">
-                <img src={watches} alt="image" />
-                <h3>WRISTWTCHES</h3>
-              </div>
-
-              <div className="first-section">
-                <img src={bangles} alt="image" />
-                <h3>BRACELETS</h3>
-              </div>
+             <div className="contents">
+               <div className="content">
+               <img src={rose} alt="image" />
             </div>
           </div>
 
-      <div className="Sectionn">
-        <h1>ARRIVALS</h1>
-
-        <div className="second-sectionns">
-          <div className="second-sectionn">
-            <img src={pin} alt="image"/>
-            <h2>KATERYNA <img src={love} className="loves"></img></h2>
-            <h4>N1,000,000</h4>
+          <div className="gall">
+            <div class="galaxy">
+             <h3>GALAXY EARRINGS <span></span> 1x</h3>
+             <h5>n950,000</h5>
+            </div>
           </div>
-
-          <div className="second-sectionn">
-            <img src={galaxy} className="styless" alt="image"/>
-            <h2>GALAXY <img src={love} className="loves"></img></h2>
-            <h4>N1,000,000</h4>
+    
+          <div className="ship">
+            <h4>SHIPPING </h4>
+            <h6>Free</h6>
           </div>
-        </div>
+    
+          <div className="total">
+            <h4>TOTAL</h4>
+            <h5>n950,000</h5>
+          </div>
+  
+      </header>
+
+      <div className="sections">
+        <h1>PAY WITH CARD</h1>
+        <input type="text" name="email" value={"Email"}/>
+        <h2>CARD INFORMATION</h2>
+        <input type="text"name="card" value={"1234 1234 1234 1234"}/>
+        <p></p>
+        <input type="text" name="expire" value={"MM / YY"}/>
+        <p></p>
+        <input type="text" name="cv" value={"CVV"}/>
+        <h3>CARDHOLDER NAME</h3>
+        <input type="text" name="name" value={"Full name on card"}/>
+        <p></p>
+        <h4>BILLING ADDRESS</h4>
+        <input type="text" name="country" value={"Nigeria"}/>
+        <p></p>
+        <input type="text" name="adress" value={"Address"}/>
+      </div>
+      
+      <div className="checks">
+         <input type="checkbox" name="check"/>
+         <label for="check">Securely save my information for 1-click checkout</label>    
+         <p></p>
+         <button className="bott"><a href="a">PURCHASE</a></button>
       </div>
 
-      <div className="Sectionss">
-        <Link className={"link-styles"} to="/">SHOW ALL ARRIVALS </Link>
-        <div className="third-section">
-          <img src={cubana} alt="image"/>
-          <h2>BEST SELLERS</h2>
-          <Link className={"styles"} to="/">EXPLORE</Link>
-        </div>
-      </div>
-
-    <div className="footers">
-      <div className="footer">
-        <button class="btn"><a href="a">SIGN UP</a></button>
-        <h1>Get the Latest Update and Offers</h1>
-        <button class="email"><a href="a">Enter Email</a></button>
-        <button class="sub"><a href="a">SUSCRIBE</a></button>
-      </div>
-
-      <div className="foot"> 
-        <div className="foots">
-          <img src={facebook} alt="image"/>
-          <img src={instagram} alt="image"/>
-        </div>
-
-        <div className="foots">
-          <h1>OUR SERVICES</h1>
-          <h4>PREMIUM SALES</h4>
-          <h4>CLASSIC SALES</h4>
-          <h4>PLATNIUM PURCHASE</h4>
-        </div>
-
-        <div className="foots">
-          <h1>PRODUCTS</h1>
-          <h4>WRIST WATCHES</h4>
-          <h4>BRACELETS</h4>
-          <h4>RINGS</h4>
-        </div>
-
-        <div className="foots">
-          <h1>POLICY</h1>
-          <h4>REFUNDS</h4>
-          <h4>LEGAL</h4>
-          <h4>ABOUT US</h4>
-          <h4>CONTACT US</h4>
-        </div>
-      </div>
-    </div>
 
 
     </div>
